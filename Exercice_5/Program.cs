ea26b4465ce5d36b9b3bf4429fad1925cc74934d
MACK Mathieu <mathieu.mack@econocom.com>
2018-09-25 11:09:52 +0200

    Add exercice 5 solution

---
+++ Exercice_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] monTableau = new int[100];
+
+            Console.WriteLine("Génération du tableau...");
+
+            for (int i = 0; i < 100; i++)
+            {
+                monTableau[i] = random.Next(1, 6);
+                Console.Write(monTableau[i] + " ");
+            }
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Nettoyage du tableau...");
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = i + 1; j < 100; j++)
+                {
+                    if (monTableau[i] == monTableau[j])
+                        monTableau[j] = -1;
+                }
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                if(monTableau[i] != -1)
+                    Console.Write(monTableau[i] + " ");
+            }
+            Console.WriteLine(" ");
+
+            Console.ReadLine();
+        }
+    }
+}
