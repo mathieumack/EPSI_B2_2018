2e5c55f8c9acddadd23bdde62dc464df3a02768c
MACK Mathieu <mathieu.mack@econocom.com>
2018-10-01 23:22:54 +0200

    Add Exercice 9

---
+++ Exercice_9/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_9
+{
+    public enum ChiFouMi
+    {
+        Pierre = 0,
+        Feuille = 1,
+        Ciseaux = 2
+    }
+
+    public class Game
+    {
+        private Random random { get; set; }
+
+        public void Start()
+        {
+            random = new Random();
+
+            ChiFouMi valeurJoueur = RecupererValeurJoueur();
+            ChiFouMi valeurApplication = RecupererValeurApplication();
+
+            CheckWinner(valeurJoueur, valeurApplication);
+        }
+
+        private void CheckWinner(ChiFouMi valeurJoueur, ChiFouMi valeurApplication)
+        {
+            if (valeurJoueur == ChiFouMi.Pierre && valeurApplication == ChiFouMi.Feuille)
+                Console.WriteLine("L'application a gagné.");
+            else if (valeurJoueur == ChiFouMi.Pierre && valeurApplication == ChiFouMi.Ciseaux)
+                Console.WriteLine("L'utilisateur a gagné.");
+            else if (valeurJoueur == ChiFouMi.Feuille && valeurApplication == ChiFouMi.Pierre)
+                Console.WriteLine("L'utilisateur a gagné.");
+            else if (valeurJoueur == ChiFouMi.Feuille && valeurApplication == ChiFouMi.Ciseaux)
+                Console.WriteLine("L'application a gagné.");
+            else if (valeurJoueur == ChiFouMi.Ciseaux && valeurApplication == ChiFouMi.Feuille)
+                Console.WriteLine("L'utilisateur a gagné.");
+            else if (valeurJoueur == ChiFouMi.Ciseaux && valeurApplication == ChiFouMi.Pierre)
+                Console.WriteLine("L'application a gagné.");
+            else
+                Console.WriteLine("Egalité.");
+        }
+
+        private ChiFouMi RecupererValeurJoueur()
+        {
+            int valeur = int.Parse(Console.ReadLine());
+            return (ChiFouMi)valeur;
+        }
+
+        private ChiFouMi RecupererValeurApplication()
+        {
+            int valeur = random.Next(0, 4);
+            return (ChiFouMi)valeur;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercice_9_1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_9_1
+{
+    public enum ChiFouMi
+    {
+        Pierre = 0,
+        Feuille = 1,
+        Ciseaux = 2,
+        Puit = 3
+    }
+
+    public interface ChiFouMiObject
+    {
+        ChiFouMi Value { get; }
+
+        /// <summary>
+        /// Renvoie vrai si l'objet courant bat la valeur passée en paramètre
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        bool Bat(ChiFouMi value);
+    }
+
+    public class Pierre : ChiFouMiObject
+    {
+        public ChiFouMi Value { get { return ChiFouMi.Pierre; } }
+
+        public bool Bat(ChiFouMi value)
+        {
+            return value == ChiFouMi.Ciseaux;
+        }
+    }
+
+    public class Feuille : ChiFouMiObject
+    {
+        public ChiFouMi Value { get { return ChiFouMi.Feuille; } }
+
+        public bool Bat(ChiFouMi value)
+        {
+            return value == ChiFouMi.Pierre || value == ChiFouMi.Puit;
+        }
+    }
+
+    public class Ciseaux : ChiFouMiObject
+    {
+        public ChiFouMi Value { get { return ChiFouMi.Ciseaux; } }
+
+        public bool Bat(ChiFouMi value)
+        {
+            return value == ChiFouMi.Feuille;
+        }
+    }
+
+    public class Puit : ChiFouMiObject
+    {
+        public ChiFouMi Value { get { return ChiFouMi.Puit; } }
+
+        public bool Bat(ChiFouMi value)
+        {
+            return value == ChiFouMi.Pierre || value == ChiFouMi.Ciseaux;
+        }
+    }
+
+    public class Game
+    {
+        private Random random { get; set; }
+
+        public void Start()
+        {
+            random = new Random();
+
+            while (true)
+            {
+                ChiFouMi valeurJoueur = RecupererValeurJoueur();
+                ChiFouMiObject valeurApplication = RecupererValeurApplication();
+
+                if (valeurJoueur == valeurApplication.Value)
+                    Console.WriteLine("Egalité.");
+                else if (valeurApplication.Bat(valeurJoueur))
+                    Console.WriteLine("L'ordinateur a gagné, il a joué : " + valeurApplication.Value.ToString());
+                else
+                    Console.WriteLine("Bravo, vous avez gagné, il a joué : " + valeurApplication.Value.ToString());
+            }
+        }
+
+        private ChiFouMi RecupererValeurJoueur()
+        {
+            Console.WriteLine("Tapez 0 pour la pierre, 1 pour la feuille, 2 pour les ciseaux et 3 pour le puit.");
+            int valeur = int.Parse(Console.ReadLine());
+            return (ChiFouMi)valeur;
+        }
+
+        private ChiFouMiObject RecupererValeurApplication()
+        {
+            ChiFouMi valeur = (ChiFouMi)random.Next(0, 4);
+
+            if (valeur == ChiFouMi.Ciseaux)
+                return CreateChiFouMiObject<Ciseaux>();
+            else if (valeur == ChiFouMi.Feuille)
+                return CreateChiFouMiObject<Feuille>();
+            else if (valeur == ChiFouMi.Pierre)
+                return CreateChiFouMiObject<Pierre>();
+            else if (valeur == ChiFouMi.Puit)
+                return CreateChiFouMiObject<Puit>();
+            else
+                return null;
+        }
+
+        private ChiFouMiObject CreateChiFouMiObject<T>() where T : ChiFouMiObject, new()
+        {
+            return new T();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tapez Ctrl+C pour quitter l'application.");
+
+            Game game = new Game();
+            game.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
